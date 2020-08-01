8525cc4b75b2472567f45f3978799fb6003b84be
Yahya-fazlani <66418922+Yahya-fazlani@users.noreply.github.com>
2020-08-01 16:03:48 +0530

    removed navbar overlayering bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,11 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("poster");
 var modalImg = document.getElementById("full screen img");
+var fixedNav = document.getElementById("fixed-nav");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
+  fixedNav.style.zIndex = 0;
 }
 
 // Get the <span> element that closes the modal
