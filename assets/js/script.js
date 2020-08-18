721fd4e83c7230d4b49797cee5b39ee33cc9f366
YahyaFazlani <yahyafazlani@outlook.com>
2020-08-18 10:35:49 +0530

    added minified files

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,23 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("poster");
 var modalImg = document.getElementById("full screen img");
 var fixedNav = document.getElementById("fixed-nav");
-img.onclick = function(){
+img.onclick = function () {
   modal.style.display = "block";
   modalImg.src = this.src;
   fixedNav.style.zIndex = 0;
-}
+};
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-var content = document.getElementsByClassName('col-lg-9')[0];
+var content = document.getElementsByClassName("col-lg-9")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
   fixedNav.style.zIndex = 1;
-}
+};
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
   fixedNav.style.zIndex = 1;
-}
+};
